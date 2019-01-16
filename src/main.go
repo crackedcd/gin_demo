d787aa2385fa00558b46ff9641839f03f19689d2
crackedcd <crackedcd@qq.com>
2019-01-16 17:36:09 +0800

    update test.

--- src/main.go
+++ src/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"./common/utils/LoggerUtils"
 	"./controller"
-	"fmt"
 	"runtime"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	router := controller.GetRouter()
-	err := router.Run(":54103"); if err != nil {
-		fmt.Println("listen failed.")
-	}
+	err := router.Run(":54103")
+	LoggerUtils.Error(err)
 }
